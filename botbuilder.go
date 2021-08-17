c7f29319efc96358046a4e40467325a56bbda6f9
baka da yo <dyatlovleonid@gmail.com>
2021-08-17 11:49:09 +0300

    added some docs

--- botbuilder.go
+++ botbuilder.go
@@ -1,11 +1,16 @@
+// Package botbuilder allows for simple Discord bot initialization utilizing the builder pattern.
+// The Discord API functionality is provided by a well-known package discordgo.
 package botbuilder
 
 import "github.com/bwmarrin/discordgo"
 
+// NewBotBuilder creates a new BotBuilder object and writes the token specified into the builder.
+// You don't have to add "Bot " to the token like in discordgo as the builder will do it for you.
 func NewBotBuilder(token string) BotBuilder {
 	return &botBuilder{token: token}
 }
 
+// BotBuilder represents a Discord bot builder.
 type BotBuilder interface {
 	AddEventHandler(func(*discordgo.Session, interface{})) BotBuilder
 	AddEventHandlers(...func(*discordgo.Session, interface{})) BotBuilder
@@ -19,21 +24,25 @@ type botBuilder struct {
 	commandHandler CommandHandler
 }
 
+// AddEventHandler adds a singular Discord event handler to the bot.
 func (bb *botBuilder) AddEventHandler(eventHandler func(*discordgo.Session, interface{})) BotBuilder {
 	bb.eventHandlers = append(bb.eventHandlers, eventHandler)
 	return bb
 }
 
+// AddEventHandlers adds multiple Discord event handlers to the bot.
 func (bb *botBuilder) AddEventHandlers(eventHandlers ...func(*discordgo.Session, interface{})) BotBuilder {
 	bb.eventHandlers = append(bb.eventHandlers, eventHandlers...)
 	return bb
 }
 
+// AddCommandHandler adds a CommandHandler to the bot.
 func (bb *botBuilder) AddCommandHandler(commandHandler CommandHandler) BotBuilder {
 	bb.commandHandler = commandHandler
 	return bb
 }
 
+// Build creates a Discord bot, adding the command handler and event handlers specified.
 func (bb *botBuilder) Build() (*discordgo.Session, error) {
 	dg, err := discordgo.New("Bot " + bb.token)
 	if err != nil {
