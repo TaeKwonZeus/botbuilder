299ec17a64f9f0e6a7ac418711dbfe5929229b57
baka <dyatlovleonid@gmail.com>
2021-08-31 15:48:13 +0300

    added the functionality

--- slashCommandHandler.go
+++ slashCommandHandler.go
@@ -1,6 +1,8 @@
 package botbuilder
 
-import "github.com/bwmarrin/discordgo"
+import (
+	"github.com/bwmarrin/discordgo"
+)
 
 type slashCommandHandler struct {
 	slashCommands        []*discordgo.ApplicationCommand
@@ -9,10 +11,15 @@ type slashCommandHandler struct {
 
 func (sch slashCommandHandler) build(session *discordgo.Session) {
 	if sch.slashCommands != nil {
+		for _, i := range sch.slashCommands {
+			session.ApplicationCommandCreate(session.State.User.ID, "", i)
+		}
 		session.AddHandler(sch.onInteractionCreate)
 	}
 }
 
 func (sch slashCommandHandler) onInteractionCreate(session *discordgo.Session, event *discordgo.InteractionCreate) {
-
+	if h, ok := sch.slashCommandHandlers[event.ApplicationCommandData().Name]; ok {
+		h(session, event)
+	}
 }
